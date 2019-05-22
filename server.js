f7077db8c2e7ebce6fa48371de169987e6ac5adc
Stroupe79 <cstroupe79@gmail.com>
2019-05-22 09:21:38 -0500

    Working server

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
-const exp = require("express")
+require('dotenv').config();
+const express = require("express")
 const path = require("path")
+const mysql = require ("mysql");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
