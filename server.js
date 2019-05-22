a1db1412c5d06e6505ff6630a417d559fdc2994c
Stroupe79 <cstroupe79@gmail.com>
2019-05-22 16:07:45 -0500

    working routing - finally

--- server.js
+++ server.js
@@ -2,30 +2,21 @@ require('dotenv').config();
 const express = require("express")
 const path = require("path")
 const mysql = require ("mysql");
-require ("./app/routing/htmlRoutes")
-require ("./app/routing/apiRoutes")
 
 var app = express();
+// require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-htmlRoutes;
 
 
 // app.get("/", function(req, res) {
 //     res.sendFile(path.join(__dirname, "./app/public/home.html"));
 //   });
-
-
-
-
-
-
-
-
-
 
 
 
